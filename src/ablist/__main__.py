6ac2100c6fadda58da83def31aae2e17474dca8b
abneeeees <avneeshsingh288@gmail.com>
2026-06-18 16:11:13 +0530

    Structure and Implementations of all the functions in the mainfile

--- src/ablist/__main__.py
+++ src/ablist/__main__.py
@@ -1,51 +1,145 @@
-from ablist.argument import *
-from ablist.file import *
+from ablist.argument import parser
+from ablist.engine import WordListModes
+from ablist.file import FileHandler
+from ablist.token import (
+    DateTimeListType,
+    NumberBasedListType,
+    PatternBasedListType,
+    TextBasedListType,
+)
 
-args = parser.parse_args()
 
-def filehandling():
-    args = parser.parse_args()
-    if len(args.output) == 1:
-        args.output.append("output.txt")
+def create_tokens(words: list) -> dict:
+    tokens = {
+        "StringTokens": [],
+        "IntegerTokens": [],
+        "DateTimeTokens": [],
+        "PatternBasedTokens": [],
+    }
 
-    if(len(args.output) > 2):
+    text_based = TextBasedListType()
+    number_based = NumberBasedListType()
+    pattern_based = PatternBasedListType()
+    date_time_based = DateTimeListType()
+
+    for word in words:
+        # Text-based tokens
+        if text_based.lowercase_tokens(word):
+            tokens["StringTokens"].append(word)
+
+        if text_based.uppercase_tokens(word):
+            tokens["StringTokens"].append(word)
+
+        if text_based.alphanumeric_tokens(word):
+            tokens["StringTokens"].append(word)
+
+        if text_based.capitalized_tokens(word):
+            tokens["StringTokens"].append(word)
+
+        if text_based.special_char_only_tokens(word):
+            tokens["StringTokens"].append(word)
+
+        # Number-based tokens
+        if number_based.int_tokens(word):
+            tokens["IntegerTokens"].append(word)
+
+        if number_based.float_tokens(word):
+            tokens["IntegerTokens"].append(word)
+
+        if number_based.short_number_tokens(word):
+            tokens["IntegerTokens"].append(word)
+
+        if number_based.long_number_tokens(word):
+            tokens["IntegerTokens"].append(word)
+
+        if number_based.padded_number_tokens(word):
+            tokens["IntegerTokens"].append(word)
+
+        # DateTime-based tokens
+        if date_time_based.is_date_ddmmyyyy(word):
+            tokens["DateTimeTokens"].append(word)
+
+        if date_time_based.is_date_yyyymmdd(word):
+            tokens["DateTimeTokens"].append(word)
+
+        if date_time_based.is_month_name(word):
+            tokens["DateTimeTokens"].append(word)
+
+        if date_time_based.is_weekday(word):
+            tokens["DateTimeTokens"].append(word)
+
+        if date_time_based.is_time(word):
+            tokens["DateTimeTokens"].append(word)
+
+        # Pattern-based tokens
+        if pattern_based.is_email_like(word):
+            tokens["PatternBasedTokens"].append(word)
+
+        if pattern_based.is_camel_case(word):
+            tokens["PatternBasedTokens"].append(word)
+
+        if pattern_based.is_snake_case(word):
+            tokens["PatternBasedTokens"].append(word)
+
+        if pattern_based.is_kebab_case(word):
+            tokens["PatternBasedTokens"].append(word)
+
+    return tokens
+
+
+def engine(tokens: dict, final_word_list: list) -> list:
+
+    return final_word_list
+
+
+def filehandling(filename_and_filetype: list, final_word_list: list) -> None:
+    if len(filename_and_filetype) == 1:
+        filename_and_filetype.append("output.txt")
+
+    if len(filename_and_filetype) > 2:
         print("Invalid number of arguments for output option.")
-        return
-    
-    if(len(args.output) == 0):
+        exit(1)
+
+    if len(filename_and_filetype) == 0:
         print("No output option provided. Defaulting to terminal output.")
-        args.output = ['D', None]
+        filename_and_filetype = ["D", None]
 
-    if(args.output[0] not in ['B', 'D', 'T']):
+    if filename_and_filetype[0] not in ["B", "D", "T"]:
         print("Invalid output type. Please choose 'B', 'D', or 'T'.")
-        return
-    
-    if(args.output[0] in ['B', 'T'] and args.output[1] is None):
+        exit(1)
+
+    if filename_and_filetype[0] in ["B", "T"] and filename_and_filetype[1] is None:
         print("Please provide a filename for the output.")
-        return
-    
-    if(args.output[0] == 'D' and args.output[1] is not None):
+        exit(1)
+
+    if filename_and_filetype[0] == "D" and filename_and_filetype[1] is not None:
         print("Filename is not required for terminal output. Ignoring the filename.")
-        args.output[1] = None
+        filename_and_filetype[1] = None
 
-    mode = args.output[0]
-    filename = args.output[1]
+    mode = filename_and_filetype[0]
+    filename = filename_and_filetype[1]
 
-    obj = FileHandler(filename)
-    if(mode == 'T'):
+    obj = FileHandler(filename, final_word_list)
+    if mode == "T":
         obj.save_as_txt()
-    elif (mode == 'D'):
-        for word in output:
+    elif mode == "D":
+        for word in final_word_list:
             print(word)
-    elif (mode  == 'B'):
+    elif mode == "B":
         obj.save_as_txt()
-        for word in output:
+        for word in final_word_list:
             print(word)
     else:
         print("Invalid output type. Please choose 'T', 'D', or 'B'.")
 
+
 def main():
-    filehandling()
+    args = parser.parse_args()
+    FinalWordList: list = []
+
+    tokens = create_tokens(args.words)
+    final_output = engine(tokens, FinalWordList)
+    filehandling(args.output, final_output)
 
 
 if __name__ == "__main__":
