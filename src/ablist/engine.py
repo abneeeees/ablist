728aa4e015c06b07173efb36159252bb536278a9
abneeeees <avneeshsingh288@gmail.com>
2026-06-19 15:50:42 +0530

    Add some more functions to the genrator

--- src/ablist/engine.py
+++ src/ablist/engine.py
@@ -1,95 +1,138 @@
-from ablist.token import  TextBasedListType, NumberBasedListType, DateTimeListType, PatternBasedListType
-from itertools import permutations, combinations_with_replacement
+from itertools import combinations_with_replacement, permutations
+
 
 class Transformer:
     def __init__(self):
         pass
 
-    def Uppercase(self, word:str) -> str:
+    def Uppercase(self, word: str) -> str:
         return word.upper()
-    
-    def Lowercase(self, word:str) -> str:
+
+    def Lowercase(self, word: str) -> str:
         return word.lower()
-    
-    def AddSuffix(self, word:str, suffix:str) -> str:
-        return word + suffix
-    
-    def AddPrefix(self, word:str, prefix:str) -> str:
-        return prefix + word
-    
-    def Capitalize(self, word:str) -> str:
+
+    def AddSuffix(self, word: str, suffix: str | int) -> str:
+        return word + str(suffix)
+
+    def AddPrefix(self, word: str, prefix: str | int) -> str:
+        return str(prefix) + word
+
+    def Capitalize(self, word: str) -> str:
         return word.capitalize()
 
-    def Title(self, word:str) -> str:
+    def Title(self, word: str) -> str:
         return word.title()
 
-    def SwapCase(self, word:str) -> str:
+    def SwapCase(self, word: str) -> str:
         return word.swapcase()
-    
-    def JoinWords(self, word1:str, word2:str) -> str:
-        return word1 + word2
-    
-    def JoinWith(self, word1:str, word2:str, separator:str) -> str:
-        return word1 + separator + word2
-    
-class Generator:
+
+    def JoinWords(self, word1: str | int, word2: str | int) -> str:
+        return str(word1) + str(word2)
+
+    def JoinWith(self, word1: str | int, word2: str | int, separator: str | int) -> str:
+        return str(word1) + str(separator) + str(word2)
+
+
+class Generator(Transformer):
     def __init__(self):
         pass
 
     # aVneEsh101 -> AvNeEsH101
-    def ToggleCase(self, word:str) -> str:
+    def ToggleCase(self, word: str) -> str:
         for x in range(len(word)):
             if x % 2 == 0:
-                word = word[:x] + word[x].upper() + word[x+1:]
+                word = word[:x] + word[x].upper() + word[x + 1 :]
             else:
-                word = word[:x] + word[x].lower() + word[x+1:]
+                word = word[:x] + word[x].lower() + word[x + 1 :]
         return word
 
-    def permutations(self, words:list, limit:int) -> list:
+    def permutations(self, words: list, limit: int) -> list:
         perm = permutations(words, limit)
-        return [''.join(p) for p in perm]
-    
-    def combinations(self, words:list, limit:int) -> list:
+        return ["".join(p) for p in perm]
+
+    def combinations(self, words: list, limit: int) -> list:
         comb = combinations_with_replacement(words, limit)
-        return [''.join(c) for c in comb]
-    
-    def LeetSpeak(self, word:str) -> str:
+        return ["".join(c) for c in comb]
+
+    def LeetSpeak(self, word: str) -> str:
         LeetDict = {
-            'a': '@',        'A': '@',
-            'e': '3',        'E': '3',
-            'i': '1',        'I': '1',
-            'o': '0',        'O': '0',
-            'l': '1',        'L': '1',
-            's': '$',        'S': '$',
-            't': '7',        'T': '7',
-            'b': '8',        'B': '8',
+            "a": "@",
+            "A": "@",
+            "e": "3",
+            "E": "3",
+            "i": "1",
+            "I": "1",
+            "o": "0",
+            "O": "0",
+            "l": "1",
+            "L": "1",
+            "s": "$",
+            "S": "$",
+            "t": "7",
+            "T": "7",
+            "b": "8",
+            "B": "8",
         }
 
-        return ''.join(LeetDict[x] if x in LeetDict else x for x in word)
+        return "".join(LeetDict[x] if x in LeetDict else x for x in word)
+
+    def AddCommonNumers(self, word: str) -> str:
+        COMMON_NUMBERS: list = [
+            "1",
+            "12",
+            "123",
+            "1234",
+            "007",
+            "69",
+            "420",
+            "777",
+            "0011",
+            "67",
+            "00",
+        ]
+
+        for num in COMMON_NUMBERS:
+            word = self.AddSuffix(word, num)
+        return word
+
+    def AddCommonSymbols(self, word1: str, word2: str) -> list:
+        COMMON_SEPARATORS = [
+            "_", "-", ".", "@", "$", "#", "&", "+", "!", "*", "~", "^", "=",
+        ]
+
+        for symbol in COMMON_SEPARATORS:
+            return [''.join(self.JoinWith(word1=word1, word2=word2, separator=symbol))]
 
 
-class WordListModes:
-    def __init__(self, final_word_list:list):
+class WordListModes(Generator):
+    def __init__(
+        self,
+        final_word_list: list,
+        string_tokens: list,
+        integer_tokens: list,
+        date_time_tokens: list,
+        pattern_based_tokens: list,
+    ):
         self.final_word_list = final_word_list
-        self.TextBasedListType = TextBasedListType()
-        self.NumberBasedListType = NumberBasedListType()
-        self.DateTimeListType = DateTimeListType()
-        self.PatternBasedListType = PatternBasedListType()
+        self.StringTokens = string_tokens
+        self.IntegerTokens = integer_tokens
+        self.DateTimeTokens = date_time_tokens
+        self.PatternBasedTokens = pattern_based_tokens
 
-    def Mode1(self):
+    def FastMode(self):
+        pass
         
-        return self.final_word_list
-    
-    def Mode2(self):
+    def SmartMode(self):
         pass
 
-    def Mode3(self):
-        pass    
-
-    def Mode4(self):
+    def AggressiveMode(self):
         pass
 
-# Mode 1 = combinations only
-# Mode 2 = combinations + permutations
-# Mode 3 = combinations + case transforms
-# Mode 4 = permutations + case transforms + separators + numbers + leet + prefixes/suffixes
+    def GodMode(self):
+        pass
+
+
+# Fast Mode: Original words and basic combinations.
+# Smart Mode: Adds permutations and common word order changes.
+# Aggressive Mode: Adds case variations, capitalization patterns, and text mutations.
+# God Mode: Applies all permutations, case mutations, leetspeak, dates, numbers, prefixes, suffixes, separators,
